d115c831d6e95deabd4f61e853877b282b94b334
aofel <yeafell@gmail.com>
2014-08-20 14:24:53 +0800

    Initial Commit

---
+++ MVVM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by aofel on 14-8-20.
+//  Copyright (c) 2014年 zyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MVVM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MVVM
+//
+//  Created by aofel on 14-8-20.
+//  Copyright (c) 2014年 zyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
